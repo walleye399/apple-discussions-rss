813e0ff50e382264e3cd6b2044ea65439467316a
walleye399 <raft.walleye-86@icloud.com>
2026-05-14 22:41:26 +0900

    Create generate-rss.js

---
+++ scripts/generate-rss.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import { create } from "xmlbuilder2";
+
+const items = JSON.parse(fs.readFileSync("public/data.json", "utf-8"));
+
+const feed = create({ version: "1.0", encoding: "UTF-8" })
+  .ele("rss", { version: "2.0" })
+  .ele("channel")
+  .ele("title").txt("Apple Discussions Japan RSS").up()
+  .ele("link").txt("https://discussionsjapan.apple.com/browse").up()
+  .ele("description").txt("Unofficial RSS feed").up();
+
+items.forEach(item => {
+  feed
+    .ele("item")
+    .ele("title").txt(item.title).up()
+    .ele("link").txt(item.url).up()
+    .ele("pubDate").txt(new Date(item.timestamp).toUTCString()).up()
+    .up();
+});
+
+const xml = feed.end({ prettyPrint: true });
+fs.writeFileSync("public/feed.xml", xml);
