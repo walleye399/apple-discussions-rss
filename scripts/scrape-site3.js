915d45804ce5b6c353ec06bab2e1bc163270495f
walleye399 <raft.walleye-86@icloud.com>
2026-07-01 20:31:40 +0900

    Create scrape-site3.js

---
+++ scripts/scrape-site3.js
@@ -0,0 +1,22 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  await page.goto("https://blog.livedoor.com/headline/", {
+    waitUntil: "networkidle0"
+  });
+
+  const items = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll("div.article")).map(el => ({
+      title: el.querySelector("h3.article-title")?.innerText || "",
+      link: el.querySelector("a.article-title-link")?.href || "",
+      date: el.querySelector("span.article-date")?.innerText || ""
+    }));
+  });
+
+  fs.writeFileSync("site3.json", JSON.stringify(items, null, 2));
+  await browser.close();
+})();
