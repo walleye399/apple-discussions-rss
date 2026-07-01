da542cf1d322f5372284663aa1c0c7ff8789653c
walleye399 <raft.walleye-86@icloud.com>
2026-07-01 20:29:16 +0900

    Add web scraping script for discussions site
    This script uses Puppeteer to scrape discussion items from a specified webpage and saves the data to a JSON file.

---
+++ scripts/scrape-site1.js
@@ -0,0 +1,22 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  await page.goto("https://discussionsjapan.apple.com/browse?sortBy=dateCreatedNewest", {
+    waitUntil: "networkidle0"
+  });
+
+  const items = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(".discussion-item")).map(el => ({
+      title: el.querySelector(".title")?.innerText || "",
+      link: el.querySelector("a")?.href || "",
+      date: el.querySelector(".date")?.innerText || ""
+    }));
+  });
+
+  fs.writeFileSync("site1.json", JSON.stringify(items, null, 2));
+  await browser.close();
+})();
